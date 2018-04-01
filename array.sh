bb888d33d0064897d0a584330df8a0a779ad1399
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-01 23:13:04 +0600

    perform basic operation on array

---
+++ array.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this script contain operation on array
+# declare array using  with space delimeter string
+NAMES=(sumon salma santo shila sumi)
+AGES=(29 27 17 13 31)
+
+# print the array
+echo "the value of array NAMES is ${NAMES[@]}"
+echo "the value of array AGES is ${AGES[@]}"
+
+# print array size
+echo "size of array NAMES is ${#NAMES[@]}"
+echo "size of array AGES IS ${#AGES[@]}"
+
+# access array element using \[\]
+NAMES[0]='new first element'
+echo "first element of array NAMES is ${NAMES[0]}"
+echo "last element of array AGES is ${AGES[${#AGES[@]}-1]}"
+
