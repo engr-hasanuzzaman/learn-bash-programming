5df45bae7bea833f3228d4b1bbc9a5049987b428
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-01 22:36:34 +0600

    write script that print passing argument

---
+++ pass_arg_to_script.sh
@@ -0,0 +1,5 @@
+echo 'Hello bash programming, how r you ? ';
+echo "your  have passed $# arguments"
+echo "all your passing args in single string $@"
+echo "args list are $1, $2, $3, $4"
+echo "current script is $0"
