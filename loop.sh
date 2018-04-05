6e01371750de202b4cd223a27e27b858621c2be9
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-05 23:33:21 +0600

    practice while loop with variable increment

--- loop.sh
+++ loop.sh
@@ -12,3 +12,12 @@ files=`ls`
 for file in ${files[@]} ; do
 	echo $file
 done
+
+# print 1 to 10 using while loop
+S_NUM=1
+E_NUM=10
+
+while [ $S_NUM -le $E_NUM ] ; do
+	echo $S_NUM
+	S_NUM=$(($S_NUM + 1))
+done
